79a36a9fdb1698ad08b4ad62a540f30a3ff4505a
vinodhasomalingam <90784302+vinodhasomalingam@users.noreply.github.com>
2022-01-08 10:43:12 +0530

    Spring boot project - Student Management System

---
+++ Springboot -Student Management  System-MYSQL/STUDENTMYSQL/src/main/java/a/StudentmysqlApplication.java
@@ -0,0 +1,13 @@
+package a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmysqlApplication.class, args);
+	}
+
+}
